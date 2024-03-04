ca6565042aef6088a090cc9239519a18f094d4d8
EnzoMReis <142607737+EnzoMReis@users.noreply.github.com>
2024-03-04 08:02:21 -0300

    Texto

---
+++ f1_main.js
@@ -0,0 +1,25 @@
+class Obj{
+    constructor(x,y,w,h,a){
+        this.x = x
+        this.y = y
+        this.w = w
+        this.h = h
+        this.a = a
+    }
+}
+
+class Enemy extends Obj{
+    atual_car(){
+           
+    }
+
+    redo(){
+
+    }
+}
+
+class Text{
+    draw_text(){
+        //códigoss
+    }
+}
